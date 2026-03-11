514ed3a7ac46ce7519077c4708679b729532b738
harish01042010 <harishjothi05@gmail.com>
2026-03-11 13:19:38 +0530

    fix cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
+app.use(cors({
+  origin: "https://toll-claim-billing.vercel.app",
+  credentials: true
+}));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
